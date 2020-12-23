bd20bc1825fafa37a365c36653e84cd49add882d
Seppe Alaerts <r0749143@student.thomasmore.be>
2020-12-23 16:42:13 +0100

    unittest fix

--- src/test/java/fact/it/customerservice/CustomerserviceUnitTests.java
+++ src/test/java/fact/it/customerservice/CustomerserviceUnitTests.java
@@ -3,8 +3,6 @@
 import fact.it.customerservice.controller.CustomerController;
 import fact.it.customerservice.model.Customer;
 import fact.it.customerservice.repository.CustomerRepository;
-import org.hamcrest.Matchers;
-import org.hamcrest.collection.IsCollectionWithSize;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
